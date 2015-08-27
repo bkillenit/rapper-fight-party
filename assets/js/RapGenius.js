b5aa4bcedc8e61ef101d5a2b6f8327427c37b159
Hack Reactor Students <accounts@hackreactor.com>
2015-08-27 12:21:01 -0700

    refactored artistsSongs hash

--- assets/js/RapGenius.js
+++ assets/js/RapGenius.js
@@ -3,12 +3,12 @@ var RapGenius = function() {
 }
 
 var artistsSongs = {
-	'kanye west': [window.throughTheWire.split('\n'), window.goldDigger.split('\n'), window.jesusWalks.split('\n'), window.stronger.split('\n')],
+	'kanye-west': [window.throughTheWire.split('\n'), window.goldDigger.split('\n'), window.jesusWalks.split('\n'), window.stronger.split('\n')],
 	'eminem': [window.loseYourself.split('\n'), window.notAfraid.split('\n'), window.theRealSlimShady.split('\n'), window.rapGod.split('\n')],
-	'rick ross': [window.boxChevy.split('\n'), window.nine.split('\n'), window.imNotAStar.split('\n'), window.holdMeBack.split('\n')],
+	'rick-ross': [window.boxChevy.split('\n'), window.nine.split('\n'), window.imNotAStar.split('\n'), window.holdMeBack.split('\n')],
 	'schoolboy q': [window.studio.split('\n'), window.collardGreens.split('\n'), window.manOfTheYear.split('\n'), window.hellOfANight.split('\n')],
 	'tupac': [window.lifeGoesOn.split('\n'), window.thugzMansion.split('\n'), window.hitEmUp.split('\n'), window.dearMama.split('\n')],
-	'snoop dogg': [window.dropItlikeItsHot.split('\n'), window.iWannaRock.split('\n'), window.youngWildFree.split('\n'), window.doggyDoggWorld.split('\n')]
+	'snoop-dogg': [window.dropItlikeItsHot.split('\n'), window.iWannaRock.split('\n'), window.youngWildFree.split('\n'), window.doggyDoggWorld.split('\n')]
 };
 
 RapGenius.prototype.getRandomSong = function(artist) {
