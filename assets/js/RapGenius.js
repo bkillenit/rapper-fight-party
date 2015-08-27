3ea9a7232762e281138cfa67adecc34c5a5aa99c
bkillenit <blaine.j.killen@gmail.com>
2015-08-26 18:51:46 -0700

    added new eminem png and started API call to get lyrics

--- assets/js/RapGenius.js
+++ assets/js/RapGenius.js
@@ -1,7 +1,24 @@
+http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song=bad
+
+var APIkey = "1c40805213e772508e1d1e73f6fa71c6";
+
 var RapGenius = function() {
 
 }
 
-RapGenius.prototype.getLyric = function() {
+var artistsSongs = {};
+
+RapGenius.prototype.getRandomSong(artist) {
+	var songs = artistsSongs[artist];
+
+
+}
+
+RapGenius.prototype.getLyric = function(artist) {
+	var baseURL = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=" + artist + "&";
+	var songStr = "song=" + this.getRandomSong(artist);
+
+	var url = baseURL + songStr;
+
 	
 }
