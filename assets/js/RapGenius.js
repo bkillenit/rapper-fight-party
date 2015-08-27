6666bc86ae08dcdfe0f4e952e3d8e10b1cd56cfd
bkillenit <blaine.j.killen@gmail.com>
2015-08-26 19:16:50 -0700

    get request with random chosen song base code written

--- assets/js/RapGenius.js
+++ assets/js/RapGenius.js
@@ -1,24 +1,35 @@
-http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song=bad
-
-var APIkey = "1c40805213e772508e1d1e73f6fa71c6";
+// example get request: http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song=bad
 
 var RapGenius = function() {
 
 }
 
-var artistsSongs = {};
+var artistsSongs = {
+	'kanye west': ['stronger', 'jesus walks', 'through the wire', 'gold digger'],
+	'eminem': ['lose yourself', 'not afraid', 'the real slim shady', 'rap god'],
+	'rick ross': ['box chevy', '911', 'i\'m not a star', 'hold me back'],
+	'schoolboy q': ['studio', 'collard greens', 'man of the year', 'hell of a night'],
+	'tupac': ['life goes on', 'thugz mansion', 'hit em up', 'dear mama'],
+	'snoop dogg': ['drop it like it\'s hot', 'i wanna rock', 'young, wild & free', 'doggy dogg world'];
+};
 
-RapGenius.prototype.getRandomSong(artist) {
+RapGenius.prototype.getRandomSong = function(artist) {
 	var songs = artistsSongs[artist];
+	var randIndex = Math.floor(Math.random() * songs.length);
 
-
+	return songs[randIndex];
 }
 
 RapGenius.prototype.getLyric = function(artist) {
-	var baseURL = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=" + artist + "&";
-	var songStr = "song=" + this.getRandomSong(artist);
+	var baseURL = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect";
 
-	var url = baseURL + songStr;
+	var requestObj = {};
+	requestObj.artist = artist;
+	requestObj.song = this.getRandomSong(artist);
 
-	
+	$.get(baseURL, requestObj).done(function(data){
+		alert(loadedData);
+	});
+
+	return;
 }
